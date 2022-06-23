fbdf15fffd85cef68d36d22b002ae1b32024efbe
Ashley Yang <ashley.minghui@gmail.com>
2022-06-23 12:43:06 -0700

    Use NewCatFormComponent state to set input value

--- src/components/NewCatForm.js
+++ src/components/NewCatForm.js
@@ -26,20 +26,41 @@ const NewCatForm = ({addCatCallback}) => {
   <form onSubmit={submitNewCatData} className="new-cat__form">
     <section>
       <h2>Add a Cat</h2>
-      <div>
+      <div className="new-cat__fields">
         <label htmlFor="name">Name</label>
-        <input name="name" id="name" onChange={handleChange}/>
+        <input 
+          name="name" 
+          id="name" 
+          value={newCatData.name} // DON'T FORGET TO SET INPUT VALUE WITH STATE
+          onChange={handleChange}
+        />
       </div>
-      <div>
+      <div className="new-cat__fields">
         <label htmlFor="breed">Breed</label>
-        <input name="breed" id="breed" onChange={handleChange}/>
-      </div><div>
-        <label htmlFor="age">Age</label>
-        <input name="age" id="age" onChange={handleChange}/>
+        <input 
+          name="breed" 
+          id="breed" 
+          value={newCatData.breed} // USE STATE TO SET INPUT VALUE
+          onChange={handleChange}
+      />
       </div>
-      <div>
+      <div className="new-cat__fields">
+        <label htmlFor="age">Age</label>
+        <input 
+          name="age" 
+          id="age" 
+          value={newCatData.age} // USE STATE TO SET INPUT VALUE
+          onChange={handleChange}
+        />
+      </div>
+      <div className="new-cat__fields">
         <label htmlFor="personality">Personality</label>
-        <input name="personality" id="personality" onChange={handleChange}/>
+        <input 
+          name="personality" 
+          id="personality" 
+          value={newCatData.personality} // USE STATE TO SET INPUT VALUE
+          onChange={handleChange}
+        />
       </div>
       <button className="button new-cat__submit" type="submit">
         Add Cat
